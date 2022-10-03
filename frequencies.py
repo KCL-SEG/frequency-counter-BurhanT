d3646ea81a8bacf9ce7694b0c71d8972b1925da1
Burhan Tekcan <burhan.tekcan@kcl.ac.uk>
2022-10-03 01:15:06 +0100

    count frequencies in list and add to dictionary #2

--- frequencies.py
+++ frequencies.py
@@ -3,13 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    for i=0 to len(items)-1:
+    for i in range(len(items)-1):
         item = items[i]
         item = str(item)
         if item in frequencies:
             item_amount = frequencies.get(item) + 1
             frequencies.pop(item)
-            frequencies.update({item: item_amount})
+            frequencies.update({item, item_amount})
         else:
-            frequencies.update({item}: 1)
+            frequencies.update({item},1)
     return frequencies
